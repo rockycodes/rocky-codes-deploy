6191eafa1d603dcf85349fbda8be2a28752427cf
Rachel Fine <rachelefine@Rachels-MacBook-Air.local>
2018-07-24 16:30:10 -0400

    fixed text styles

--- bundle.js
+++ bundle.js
@@ -223,7 +223,7 @@ function (_React$Component) {
         className: "icon",
         src: _linkedin.default
       })), _react.default.createElement("a", {
-        href: "https://twitter.com/ballerinadino?lang=en",
+        href: "https://twitter.com/rockycodes",
         target: "_blank"
       }, _react.default.createElement("img", {
         className: "icon",
@@ -275,11 +275,17 @@ var About = function About() {
   }), _react.default.createElement("div", {
     id: "text"
   }, _react.default.createElement("p", {
-    className: "title"
-  }, "hi, i'm rocky"), _react.default.createElement("p", null, "i'm an educator and social worker turned web developer based in nyc!"), _react.default.createElement("p", null, "i've always loved math, logic, and building things and, after ten years in human services, i decided to reconnect to my left brain and think about other ways to make a difference in the world."), _react.default.createElement("p", null, "from the first time i sat down to write a for loop, i was hooked. programming combined my love of problem solving with my love for creating."), _react.default.createElement("p", null, "i made the full career switch to developer by attending the Grace Hopper program in NYC, a tuition deferred coding bootcamp for women and non-binary folks."), _react.default.createElement("p", null, "you can currently find me coding around nyc and manhattan at various meetups. i'm especially interested in the intersection of tech and social justice and the ways in which well-intentioned technology can go so very wrong."), _react.default.createElement("p", null, "connect with me on github, linkedin, or twitter, or send me a message here!"), _react.default.createElement("p", {
-    className: "header title"
+    id: "title"
+  }, "hi, i'm rocky"), _react.default.createElement("p", null, "i'm an educator and social worker turned web developer based in nyc!"), _react.default.createElement("p", null, "i've always loved math, logic, and building things and, after ten years in human services, i decided to reconnect to my left brain and think about other ways to make a difference in the world."), _react.default.createElement("p", null, "from the first time i sat down to write a for loop, i was hooked. programming combined my love of problem solving with my love for creating."), _react.default.createElement("p", null, "i made the full career switch to developer by attending the Grace Hopper program in NYC, a tuition deferred coding bootcamp for women and non-binary folks."), _react.default.createElement("p", null, "you can currently find me coding around nyc and manhattan at various meetups. i'm especially interested in the intersection of tech and social justice and the ways in which well-intentioned technology can go so very wrong."), _react.default.createElement("p", null, "connect with me on ", _react.default.createElement("a", {
+    href: "https://github.com/rockycodes/"
+  }, "github"), ", ", _react.default.createElement("a", {
+    href: "https://www.linkedin.com/in/rachelefine/"
+  }, "linkedin"), ", or ", _react.default.createElement("a", {
+    href: "https://twitter.com/rockycodes"
+  }, "twitter"), ", or send me a message here!"), _react.default.createElement("p", {
+    className: "header"
   }, "technology"), _react.default.createElement("ul", null, _react.default.createElement("li", null, "comfortable:", _react.default.createElement("br", null), "Javascript, Sequelize, Express, Node.js, React, Redux, Git, HTML, CSS"), _react.default.createElement("li", null, "familiar:", _react.default.createElement("br", null), "jQuery, OAuth, Heroku, Webpack, Postgress, Swift, Xcode"), _react.default.createElement("li", null, "acquainted:", _react.default.createElement("br", null), "SQL, Socket.io, Mocha, Chai")), _react.default.createElement("p", {
-    className: "header title"
+    className: "header"
   }, "fun facts"), _react.default.createElement("ul", null, _react.default.createElement("li", null, "i love circus arts and am an amateur aerialist and juggler"), _react.default.createElement("li", null, "road trips are my jam. i once took a two month road trip and drove 12,000 miles across the US and Canada, just me and my dog"), _react.default.createElement("li", null, "i'm an avid reader and especially love dystopic and science fiction"), _react.default.createElement("li", null, "i believe deeply in social justice, activism, and advocacy"), _react.default.createElement("li", null, "i love making things! from coding, to knitting, to matchbox dioramas, i always have some type of craftiness going on"))));
 };
 
